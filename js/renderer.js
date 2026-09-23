c1f6b3f622e81156711fde184670204085a68c35
Santiago Gonzalez <santiagoalexgonzalezc@gmail.com>
2026-09-23 09:27:44 -0400

    Update renderer.js

--- js/renderer.js
+++ js/renderer.js
@@ -1,1 +1,29 @@
+import {
+    canvas,
+    ctx,
+    camera,
+    viewport
+} from "./state.js";
 
+import {
+    drawMap
+} from "./world.js";
+
+import {
+    drawPlayer
+} from "./player.js";
+
+import {
+    updateProjectiles
+} from "./projectile.js";
+
+import {
+    drawHUD
+} from "./hud.js";
+
+export function resizeCanvas() {
+
+    viewport.width =
+        window.innerWidth;
+
+    viewport.height =
